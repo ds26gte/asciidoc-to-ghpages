14ac604f775be443c669fc554da610d73884a9e3
Dorai Sitaram <ds26gte@yahoo.com>
2019-03-15 14:55:09 -0400

    ...

---
+++ build-something.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env racket
+
+#lang racket
+
+(call-with-output-file "example.html"
+  (lambda (o)
+    (fprintf o "<!DOCTYPE html>
+    <html lang=\"en\">
+    <head>
+    </head>
+    <body>
+    <h1>Nothing special</h1>
+    <p>Nothing special, really!</p>
+    </body>
+    </html>")
+
---
+++ travis-push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git config --global user.email "${GH_USER_EMAIL}"
+git config --global user.name "${GH_USER_NAME}"
+
+git commit -m "committed at $(date)" example.html
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
