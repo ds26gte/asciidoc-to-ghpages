0566fc1e553cfcc6eb1d89d38d8cea0ad76425cd
Dorai Sitaram <ds26gte@yahoo.com>
2019-03-15 15:03:59 -0400

    ...

--- travis-push.sh
+++ travis-push.sh
@@ -3,6 +3,7 @@
 git config --global user.email "${GH_USER_EMAIL}"
 git config --global user.name "${GH_USER_NAME}"
 
+git add example.html
 git commit -m "committed at $(date)" example.html
 
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
