b4efda747f4918d7d99ed314cfb0c1e722b3779a
Dorai Sitaram <ds26gte@yahoo.com>
2019-03-15 22:31:46 -0400

    ...

--- travis-push.sh
+++ travis-push.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo DOING travis-push.sh
-
 git checkout -B travistmp
 
 git config --global user.email "${GH_USER_EMAIL}"
@@ -9,11 +7,6 @@ git config --global user.name "${GH_USER_NAME}"
 
 git add *.css *.html *.pdf
 
-echo Added example
-git commit -m "committed at $(date)" 
-
-echo Committed example
+git commit -m "committed at $(date)"
 
 git push --force "https://${GH_TOKEN}@${GH_REF}" travistmp:gh-pages > /dev/null 2>&1
-
-echo pushed!
